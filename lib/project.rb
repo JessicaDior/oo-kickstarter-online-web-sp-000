21957880881d2e5511508f0b091422c2c2d99219
Jc Dior <jessicaclairdior@gmail.com>
2020-09-25 01:45:54 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,13 @@
 class Project
-  attr_reader :backer, :title
+  attr_reader :backers, :title
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-def add_backer(backers)
-  @backers << backers
+def add_backer(backer)
+  @backers << backer
 end
 
 end
